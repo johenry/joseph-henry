05a408ef159ef4353830b172c480fbce8f8b43ac
Joe Henry <johenry@me.com>
2020-09-20 13:23:08 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Joseph Henry's Personal Site
---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<style>
+	h1 {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+		color: brown;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Joseph Henry</title>
+</svelte:head>
+
+<h1>Joseph Henry</h1>
